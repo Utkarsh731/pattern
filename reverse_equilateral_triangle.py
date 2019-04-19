9548cbb64a4591bb79eb255361e5f7c99193b4c6
UTKARSH SHUKLA <utkarsh.shukla_cs16@gla.ac.in>
2019-04-19 11:42:16 +0530

    Create reverse_equilateral_triangle.py

---
+++ reverse_equilateral_triangle.py
@@ -0,0 +1,10 @@
+n=int(input("enter the num of rows"))
+for i in range(0,n):
+    print(" "*i,end="")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+    /*  * * *
+         * *
+          *
+          */
